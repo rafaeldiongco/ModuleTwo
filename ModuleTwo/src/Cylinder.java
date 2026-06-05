207481dce753580ee8dc6f70eeef63b73093370c
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-06-05 18:39:44 +0800

    Pasted template from autochecker for Cylinder

--- ModuleTwo/src/Cylinder.java
+++ ModuleTwo/src/Cylinder.java
@@ -1,2 +1,39 @@
+// UNDERSTAND: This class models a right circular cylinder
+
 public class Cylinder {
+
+    // Data fields
+
+
+
+
+
+    // Constructor
+
+
+
+
+    // Getters
+
+
+
+
+    // Setters with validation
+
+
+
+
+
+
+
+
+
+    // UNDERSTAND: Total Surface Area of cylinder = 2πr(r + h)
+
+
+
+    // UNDERSTAND: Volume of cylinder = πr²h
+
+
+
 }
