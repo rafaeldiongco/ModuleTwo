a6ccfb83bbccdbb99e4bb9c79e9eefa4536ae530
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-06-05 18:07:12 +0800

    Created Sphere class with template from autochecker

---
+++ ModuleTwo/src/Sphere.java
@@ -0,0 +1,33 @@
+// UNDERSTAND: This class models a sphere (perfectly round 3D object)
+// DECISION: Follows same pattern as Circle class but for 3D
+public class Sphere {
+
+    // Data field
+
+
+
+
+    // Constructor
+
+
+
+
+    // Getter
+
+
+
+
+    // Setter with validation
+
+
+
+
+    // UNDERSTAND: Surface Area of sphere = 4πr²
+
+
+
+    // UNDERSTAND: Volume of sphere = (4/3)πr³
+
+
+
+}
