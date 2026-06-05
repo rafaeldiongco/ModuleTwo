e94444565e31a6137729bea1be2512f813481999
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-06-05 18:19:46 +0800

    pasted template from autochecker for cone

--- ModuleTwo/src/Cone.java
+++ ModuleTwo/src/Cone.java
@@ -1,2 +1,46 @@
+// UNDERSTAND: This class models a right circular cone
+// DECISION: Slant height can be calculated from radius and height using Pythagorean theorem
 public class Cone {
+
+    // Data fields
+
+
+
+
+
+    // Constructor
+
+
+
+
+
+    // Getters
+
+
+
+
+
+    // Setters with validation
+
+
+
+
+
+
+
+
+    // UNDERSTAND: Calculates slant height using Pythagorean theorem: l = √(r² + h²)
+
+
+
+    // UNDERSTAND: Total Surface Area of cone = πr(r + l) where l is slant height
+
+
+
+
+
+    // UNDERSTAND: Volume of cone = (1/3)πr²h
+
+
+
 }
